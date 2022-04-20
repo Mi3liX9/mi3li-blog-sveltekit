4a5660dd904ac33660d93d1acbe6e99ba8f479a8
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-20 16:36:01 +0300

    add class type

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let telegram: TelegramWebApp;
+	let theme: 'light' | 'dark';
+	onMount(() => {
+		telegram = window.Telegram.WebApp;
+		theme = document.documentElement.className as 'light' | 'dark';
+	});
+</script>
+
 <article class="prose dark:prose-invert">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 	<button>Hello</button>
+
+	<pre>{JSON.stringify({ telegram, theme }, null, 2)}</pre>
 </article>
