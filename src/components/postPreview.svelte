58a2fb26fedefa03aa89cc47929ca498df0710a6
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-21 13:14:06 +0300

    add primary color and put it in ring

--- src/components/postPreview.svelte
+++ src/components/postPreview.svelte
@@ -5,7 +5,7 @@
 	export let date: string | Date;
 </script>
 
-<a href={'/posts/' + slug} class="link">
+<a href={'/posts/' + slug} class="link active:scale-95 focus:ring ring-primary/50 ">
 	<div class="flex gap-2 h-16 bg-gray-200 dark:bg-black/40 p-2 not-prose select-none rounded-md">
 		<img src={pic} alt={title} class="h-full aspect-square rounded-md" />
 		<div class="flex-1">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
-			colors: {},
+			colors: {
+				primary: 'var(--button-color)'
+			},
 			typography: {
 				DEFAULT: {
 					css: {
